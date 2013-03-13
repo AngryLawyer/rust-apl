68422fed0e74093f7681cbf38cf3adf50e5f106b
Tony Aldridge <tony@dabapps.com>
2013-03-13 09:30:12 +0000

    Sensiblised tests a bit

--- src/tests.rs
+++ src/tests.rs
@@ -1,25 +1,29 @@
-
 mod tokenizer {
+
+    fn test_assert(value: bool, message: ~str) {
+        if !value {
+            fail!(message);
+        }
+    }
 
     use tokenizer;
     use tokenizer::Tokenizer;
 
-    #[test]
     fn test_tokenize() {
         test_tokenize_number();
         test_tokenize_newlines();
         test_tokenize_strings();
     }
 
+    #[test]
     fn test_tokenize_number() {
         for ([~"1", ~"321", ~"3.21", ~".21", ~"0.21", ~"¯321"]).each |number| {
             let mut tokenizer = Tokenizer::new(copy *number);
             match tokenizer.read_next_token() {
                 result::Ok(tokenizer::Number(tokenData)) => {
-                    io::println(fmt!("Read %s expected %s ", tokenData.string, *number));
 
                     //Pass
-                    fail_unless!(tokenData.string == *number);
+                    test_assert(tokenData.string == *number, fmt!("Read %s expected %s ", tokenData.string, *number));
                     fail_unless!(tokenData.row == 0);
                     fail_unless!(tokenData.col == 0);
                 },
@@ -37,8 +41,7 @@ mod tokenizer {
         match tokenizer.read_next_token() {
             result::Ok(tokenizer::Number(tokenData)) => {
                 //Pass
-                io::println(fmt!("Read %s expected %s ", tokenData.string, expected));
-                fail_unless!(tokenData.string == expected);
+                test_assert(tokenData.string == expected, fmt!("Read %s expected %s ", tokenData.string, expected));
                 /*fail_unless!(tokenData.row == 0);
                 fail_unless!(tokenData.col == 1);*/
             },
@@ -57,8 +60,7 @@ mod tokenizer {
                 result::Ok(tokenizer::Number(tokenData)) => {
                     fail!(fmt!("Unexpectedly read %s from source %s",tokenData.string, *number));
                 },
-                result::Err(msg) => {
-                    io::println(fmt!("Correctly got error %s from %s", msg, *number));
+                result::Err(_msg) => {
                 },
                 _ => {
                     fail!(~"Unexpected token type");
@@ -105,12 +107,12 @@ mod tokenizer {
         let string = ~"⎕SI";*/
     }
 
+    #[test]
     fn test_tokenize_newlines() {
         for ([~"\n", ~"  \n", ~"\n\n", ~"⍝ lol\n", ~"\r", ~"\r\n", ~"\r\r"]).each |newline| {
             let mut tokenizer = Tokenizer::new(copy *newline);
             match tokenizer.read_next_token() {
                 result::Ok(tokenizer::Newline(tokenData)) => {
-                    io::println("Read newline");
                     //Pass
                     fail_unless!(tokenData.row == 0);
                     fail_unless!(tokenData.col == 0);
@@ -125,6 +127,7 @@ mod tokenizer {
         }
     }
 
+    #[test]
     fn test_tokenize_strings() {
         //Standard strings
         for ([(~"'Hello'", ~"Hello"),
@@ -141,15 +144,14 @@ mod tokenizer {
             let mut tokenizer = Tokenizer::new(string);
             match tokenizer.read_next_token() {
                 result::Ok(tokenizer::String(tokenData)) => {
-                    io::println(fmt!("Read %s expected %s ", tokenData.string, result));
+                    test_assert(tokenData.string == result, fmt!("Read %s expected %s ", tokenData.string, result));
 
                     //Pass
-                    fail_unless!(tokenData.string == result);
                     fail_unless!(tokenData.row == 0);
                     fail_unless!(tokenData.col == 0);
                 },
                 result::Err(msg) => {
-                    fail!(fmt!("Expected string - %s", msg));
+                    fail!(fmt!("Expected string for %s - %s", tokenizer.char_reader.source, msg));
                 },
                 _ => {
                     fail!(~"Unexpected token type");
