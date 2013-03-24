03b50ec7f163f1d2e653af1bdb09aa3fc385ddbc
Tony Aldridge <zaragopha@hotmail.com>
2013-03-24 22:22:32 +0000

    Floats coming out correct

--- src/test_eval.rs
+++ src/test_eval.rs
@@ -16,11 +16,25 @@ fn test_eval(input: ~str, f: &fn(result: ~eval::Value)) {
 }
 
 #[test]
-fn test_eval_number() {
+fn test_eval_int() {
     do test_eval(~"3") |result| {
         match result {
-            ~eval::Integer(3) => {
-                //OK
+            ~eval::Integer(x) => {
+                assert_eq!(x, 3);
+            },
+            _ => {
+                fail!(~"Didn't find a number");
+            }
+        }
+    }
+}
+
+#[test]
+fn test_eval_float() {
+    do test_eval(~".2") |result| {
+        match result {
+            ~eval::Float(x) => {
+                assert_eq!(x, 0.2f);
             },
             _ => {
                 fail!(~"Didn't find a number");
