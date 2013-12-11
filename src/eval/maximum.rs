56ac208b02b84f4baf225f76808d5f28d5c28389
Tony Aldridge <zaragopha@hotmail.com>
2013-12-11 21:45:56 +0000

    Max and min

--- src/eval/maximum.rs
+++ src/eval/maximum.rs
@@ -1,18 +1,16 @@
-use std::result;
-
 use nodes;
 use eval::eval::{AplFloat, AplInteger, AplComplex, AplArray, Value, eval_dyadic};
 use eval::array_helpers::{simple_dyadic_array, dual_dyadic_array, inverse_simple_dyadic_array};
 
-fn maximum_float(f: &f64, other:&Value) -> result::Result<~Value, ~str> {
+fn maximum_float(f: f64, other:&Value) -> Result<~Value, ~str> {
     match other {
         &AplFloat(val) => {
-            result::Ok(~AplFloat(if *f > val { *f } else { val }))
+            Ok(~AplFloat(if f > val { f } else { val }))
         },
         &AplInteger(val) => {
-            result::Ok((if *f > val as f64 { ~AplFloat(*f) } else { ~AplInteger(val) }))
+            Ok((if f > val as f64 { ~AplFloat(f) } else { ~AplInteger(val) }))
         },
-        &AplComplex(ref _i, ref _j) => {
+        &AplComplex(_c) => {
             Err(~"Maximum is not supported on complex numbers")
         },
         &AplArray(_, _, _) => {
@@ -21,15 +19,15 @@ fn maximum_float(f: &f64, other:&Value) -> result::Result<~Value, ~str> {
     }
 }
 
-fn maximum_integer(i: &int, other:&Value) -> result::Result<~Value, ~str> {
+fn maximum_integer(i: int, other:&Value) -> Result<~Value, ~str> {
     match other {
         &AplFloat(val) => {
-            result::Ok((if *i as f64 > val { ~AplInteger(*i) } else { ~AplFloat(val) }))
+            Ok((if i as f64 > val { ~AplInteger(i) } else { ~AplFloat(val) }))
         },
         &AplInteger(val) => {
-            result::Ok(~AplInteger(if *i > val { *i } else { val }))
+            Ok(~AplInteger(if i > val { i } else { val }))
         },
-        &AplComplex(ref _i, ref _j) => {
+        &AplComplex(_c) => {
             Err(~"Maximum is not supported on complex numbers")
         },
         &AplArray(_, _, _) => {
@@ -38,9 +36,9 @@ fn maximum_integer(i: &int, other:&Value) -> result::Result<~Value, ~str> {
     }
 }
 
-fn maximum_array(array: &Value, other: &Value) -> result::Result<~Value, ~str> {
+fn maximum_array(array: &Value, other: &Value) -> Result<~Value, ~str> {
     match other {
-        &AplFloat(_) |  &AplInteger(_) | &AplComplex(_, _) => {
+        &AplFloat(_) |  &AplInteger(_) | &AplComplex(_) => {
             inverse_simple_dyadic_array(maximum, array, other)
         },
         &AplArray(_, _, _) => {
@@ -49,15 +47,15 @@ fn maximum_array(array: &Value, other: &Value) -> result::Result<~Value, ~str> {
     }
 }
 
-pub fn maximum(first: &Value, other: &Value) -> result::Result<~Value, ~str> {
+pub fn maximum(first: &Value, other: &Value) -> Result<~Value, ~str> {
     match first{
         &AplFloat(f) => {
-            maximum_float(&f, other)
+            maximum_float(f, other)
         },
         &AplInteger(i) => {
-            maximum_integer(&i, other)
+            maximum_integer(i, other)
         }
-        &AplComplex(ref _i, ref _j) => {
+        &AplComplex(_c) => {
             Err(~"Maximum is not supported on complex numbers")
         },
         &AplArray(ref _depth, ref _dimensions, ref _values) => {
@@ -66,7 +64,7 @@ pub fn maximum(first: &Value, other: &Value) -> result::Result<~Value, ~str> {
     }
 }
 
-pub fn eval_maximum(left: &nodes::Node, right: &nodes::Node) -> result::Result<~Value, ~str> {
+pub fn eval_maximum(left: &nodes::Node, right: &nodes::Node) -> Result<~Value, ~str> {
     eval_dyadic(maximum, left, right)
 }
 
--- src/eval/minimum.rs
+++ src/eval/minimum.rs
@@ -1,18 +1,19 @@
 use std::result;
 
 use nodes;
+use nodes;
 use eval::eval::{AplFloat, AplInteger, AplComplex, AplArray, Value, eval_dyadic};
 use eval::array_helpers::{simple_dyadic_array, dual_dyadic_array, inverse_simple_dyadic_array};
 
-fn minimum_float(f: &f64, other:&Value) -> result::Result<~Value, ~str> {
+fn minimum_float(f: f64, other:&Value) -> Result<~Value, ~str> {
     match other {
         &AplFloat(val) => {
-            result::Ok(~AplFloat(if *f < val { *f } else { val }))
+            Ok(~AplFloat(if f < val { f } else { val }))
         },
         &AplInteger(val) => {
-            result::Ok(if *f < val as f64 { ~AplFloat(*f) } else { ~AplInteger(val) })
+            Ok(if f < val as f64 { ~AplFloat(f) } else { ~AplInteger(val) })
         },
-        &AplComplex(ref _i, ref _j) => {
+        &AplComplex(_c) => {
             Err(~"minimum is not supported on complex numbers")
         },
         &AplArray(_, _, _) => {
@@ -21,15 +22,15 @@ fn minimum_float(f: &f64, other:&Value) -> result::Result<~Value, ~str> {
     }
 }
 
-fn minimum_integer(i: &int, other:&Value) -> result::Result<~Value, ~str> {
+fn minimum_integer(i: int, other:&Value) -> Result<~Value, ~str> {
     match other {
         &AplFloat(val) => {
-            result::Ok(if (*i as f64) < val { ~AplInteger(*i) } else { ~AplFloat(val) })
+            Ok(if (i as f64) < val { ~AplInteger(i) } else { ~AplFloat(val) })
         },
         &AplInteger(val) => {
-            result::Ok(~AplInteger(if *i < val { *i } else { val }))
+            Ok(~AplInteger(if i < val { i } else { val }))
         },
-        &AplComplex(ref _i, ref _j) => {
+        &AplComplex(_c) => {
             Err(~"minimum is not supported on complex numbers")
         },
         &AplArray(_, _, _) => {
@@ -38,9 +39,9 @@ fn minimum_integer(i: &int, other:&Value) -> result::Result<~Value, ~str> {
     }
 }
 
-fn minimum_array(array: &Value, other: &Value) -> result::Result<~Value, ~str> {
+fn minimum_array(array: &Value, other: &Value) -> Result<~Value, ~str> {
     match other {
-        &AplFloat(_) |  &AplInteger(_) | &AplComplex(_, _) => {
+        &AplFloat(_) |  &AplInteger(_) | &AplComplex(_) => {
             inverse_simple_dyadic_array(minimum, array, other)
         },
         &AplArray(_, _, _) => {
@@ -49,15 +50,15 @@ fn minimum_array(array: &Value, other: &Value) -> result::Result<~Value, ~str> {
     }
 }
 
-pub fn minimum(first: &Value, other: &Value) -> result::Result<~Value, ~str> {
+pub fn minimum(first: &Value, other: &Value) -> Result<~Value, ~str> {
     match first{
         &AplFloat(f) => {
-            minimum_float(&f, other)
+            minimum_float(f, other)
         },
         &AplInteger(i) => {
-            minimum_integer(&i, other)
+            minimum_integer(i, other)
         }
-        &AplComplex(ref _i, ref _j) => {
+        &AplComplex(_c) => {
             Err(~"minimum is not supported on complex numbers")
         },
         &AplArray(ref _depth, ref _dimensions, ref _values) => {
@@ -66,6 +67,6 @@ pub fn minimum(first: &Value, other: &Value) -> result::Result<~Value, ~str> {
     }
 }
 
-pub fn eval_minimum(left: &nodes::Node, right: &nodes::Node) -> result::Result<~Value, ~str> {
+pub fn eval_minimum(left: &nodes::Node, right: &nodes::Node) -> Result<~Value, ~str> {
     eval_dyadic(minimum, left, right)
 }
