e1cd91caed6e581f6e248615ede165e1b6c47310
Tony Aldridge <zaragopha@hotmail.com>
2013-12-13 19:36:41 +0000

    Ported reciprocal

--- src/eval/reciprocal.rs
+++ src/eval/reciprocal.rs
@@ -1,13 +1,12 @@
 use nodes;
 
-use std::result;
 use eval::eval::{Value, eval_monadic};
 use eval::divide::divide_integer;
 
-pub fn reciprocal(first: &Value) -> result::Result<~Value, ~str> {
-    divide_integer(&1, first)
+pub fn reciprocal(first: &Value) -> Result<~Value, ~str> {
+    divide_integer(1, first)
 }
 
-pub fn eval_reciprocal(left: &nodes::Node) -> result::Result<~Value, ~str> {
+pub fn eval_reciprocal(left: &nodes::Node) -> Result<~Value, ~str> {
     eval_monadic(reciprocal, left)
 }
